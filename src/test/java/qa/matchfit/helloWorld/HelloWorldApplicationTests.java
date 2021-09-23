20c90160fff982f7eac0cdf6514006c9c9b3fca0
Korbinian Ring <kmr.ring@outlook.com>
2021-09-23 12:49:01 +0100

    first commit

---
+++ src/test/java/qa/matchfit/helloWorld/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package qa.matchfit.helloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
